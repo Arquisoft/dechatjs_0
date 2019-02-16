28d6bf03784769790709c61bc1a13623e5f6c83b
Jose Emilio Labra Gayo <jelabra@gmail.com>
2019-02-16 07:48:35 +0100

    Incremented age

--- test/personTest.js
+++ test/personTest.js
@@ -8,6 +8,12 @@ describe('person', function () {
     assert.equal(foo.age, 0)
   })
 
+  it('can set age', function () {
+    const foo = new Person('foo')
+    foo.setAge(3)
+    assert.equal(foo.age, 3)
+  })
+
   it('keeps its name', function () {
     const foo = new Person('foo')
     assert.equal(foo.name, 'foo')
